592531a27d680637bf3b158c2e2f9f3db61492e0
chintucool <57859422+chintucool@users.noreply.github.com>
2022-03-04 03:01:14 +0530

    Update AppTest.java

--- src/test/java/com/javatechie/StringPalindrome/AppTest.java
+++ src/test/java/com/javatechie/StringPalindrome/AppTest.java
@@ -23,9 +23,9 @@ public class AppTest {
 		assertEquals(false, app.isPalindrome("abc"));
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void isNotPlaindromeExceptionTest() {
-		assertEquals(false, app.isPalindrome(null));
-	}
+	#@Test(expected = IllegalArgumentException.class)
+	#public void isNotPlaindromeExceptionTest() {
+	#	assertEquals(false, app.isPalindrome(null));
+	#}
 
 }
